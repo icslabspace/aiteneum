fa8911149d5642490be2934a990bc5c1fc5ec9e7
Alex Gherega <alex.gherega@gmail.com>
2018-09-19 17:58:20 +0300

    neanderthal

--- src/aiteneum/core.clj
+++ src/aiteneum/core.clj
@@ -5,28 +5,45 @@
 
 (reset! evv/vocab {})
 
-(def docs (read-string (slurp "resources/docs.txt")))
+;;(def docs (read-string (slurp "resources/docs.txt")))
 
-(def i-bow (eva.bag-of-words/docs->indexed-bows docs))
+;;(def i-bow (eva.bag-of-words/docs->indexed-bows docs))
+
+;; (def params {:ctrl {:counter 0.0
+;;                     :m-iters 100
+;;                     :num-iters 100
+;;                     :epsilon 1e-100
+;;                     :mean-thresh 1e-35}
+;;              :model {:alpha (/ 1.0 8.0)
+;;                      :eta (/ 1.0 8.0)
+;;                      :tau 1.0
+;;                      :kappa 0.5
+;;                      :num-topics 8
+;;                      :estimated-num-docs (count docs)
+;;                      :dict {:num-words (count @evv/vocab)}
+;;                      :gamma {:shape 100
+;;                              :scale 1e-2}}})
+
+;(def olda-model (olda/train params i-bow))
+
+;;(def i-bow (eva.bag-of-words/files->indexed-bows "resources/basic-corpus/"))
+(def i-bow (olda.em-3/docs->neanderthal (eva.bag-of-words/files->indexed-bows "../cambioscience/yhi/resources/data/optu/")))
 
 (def params {:ctrl {:counter 0.0
                     :m-iters 100
                     :num-iters 100
                     :epsilon 1e-100
-                    :mean-thresh 1e-35}
+                    :mean-thresh 1e-135}
              :model {:alpha (/ 1.0 8.0)
                      :eta (/ 1.0 8.0)
                      :tau 1.0
-                     :kappa 0.5
+                     :kappa 0.55
                      :num-topics 8
-                     :estimated-num-docs (count docs)
+                     :estimated-num-docs (count i-bow)
                      :dict {:num-words (count @evv/vocab)}
                      :gamma {:shape 100
                              :scale 1e-2}}})
 
-;(def olda-model (olda/train params i-bow))
-
-;(def i-bow (eva.bag-of-words/files->indexed-bows "../cambioscience-all/selena/resources/data/single-doc/"))
 
 ;(def i-bow (eva.bag-of-words/files->indexed-bows "resources/"))
 

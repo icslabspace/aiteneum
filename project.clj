dc91d2ffd174ec0ff482edb6378f76825e76eafe
alex-gherega <alex.gherega@gmail.com>
2019-09-15 09:14:58 +0300

    finally adding deps

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [net.cailuno/denisovan "0.1.1"]
                  [org.clojure/clojure "1.9.0"]
                  [generateme/fastmath "0.1.1"]
-                 [net.mikera/core.matrix "0.62.0"]
+                 ;[net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]
                  [midje "1.9.2"]
                  [criterium "0.4.4"]
